0238e7a5f2000942ee3d645692dfbef50b42ed57
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-02-26 16:48:28 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,14 +18,35 @@ public class Main {
         }
         
         Bowl.addFood(10);
-    
+        System.out.println();
+        
         sharik.run(100);
         sharik.swim(100);
         cats[0].run(100);
         cats[0].swim(100);
+        System.out.println();
         
         System.out.println("Животных создано: " + Animal.countAnimal);
         System.out.println("Собак создано: " + Dog.countDog);
         System.out.println("Котов создано: " + Cat.countCat);
+        System.out.println();
+        
+        Circle circle = new Circle(5);
+        circle.setFillColor("Красный");
+        circle.setBorderColor("Чёрный");
+        circle.printCharacteristics();
+        System.out.println();
+        
+        Rectangle rectangle = new Rectangle(4, 6);
+        rectangle.setFillColor("Синий");
+        rectangle.setBorderColor("Зелёный");
+        rectangle.printCharacteristics();
+        System.out.println();
+        
+        Triangle triangle = new Triangle(3, 4, 5);
+        triangle.setFillColor("Жёлтый");
+        triangle.setBorderColor("Оранжевый");
+        triangle.printCharacteristics();
+        System.out.println();
     }
 }
