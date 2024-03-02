5598f2ced5a34075c4f00551015bccc991a1aba2
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-02 20:22:43 +0400

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,18 +1,22 @@
 public class Cat extends Animal {
     private String name;
     private boolean isFull;
+    private int appetite;
     public static int countCat = 0;
+    private final int maxRun;
  
     Cat (String name) {
         super();
         countCat++;
         this.name = name;
         this.isFull = false;
+        this.appetite = 5;
+        this.maxRun = 200;
     }
  
     @Override
     public void run(int range) {
-        if (range > 200) {
+        if (range > maxRun) {
             System.out.println(name + " не может пробежать " + range + "метров.");
         } else {
             System.out.println(name + " пробежал " + range + " метров.");
@@ -25,12 +29,13 @@ public class Cat extends Animal {
     }
 
     public void eat() {
-    if (isFull == false && Bowl.food > 0) {
+    if (isFull == false && Bowl.food >= appetite) {
         System.out.println(name + " поел.");
         isFull = true;
-        Bowl.decreaseFood(5);
+        Bowl.decreaseFood(appetite);
     } else {
         System.out.println(name + " голоден.");
+        Bowl.decreaseFood(appetite);
         }
     }
     
