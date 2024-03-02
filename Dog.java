22d0ed1be8323ff15f41f631ac8ca77c42615f60
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-02 20:24:04 +0400

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,16 +1,20 @@
 public class Dog extends Animal{
     private String name;
     public static int countDog = 0;
+    private final int maxRun;
+    private final int maxSwim;
     
     Dog(String name) {
         super();
         countDog++;
         this.name = name;
+        this.maxRun = 500;
+        this.maxSwim = 10;
     }
  
     @Override
     public void run(int range) {
-        if (range > 500) {
+        if (range > maxRun) {
             System.out.println("Собака не может пробежать " + range + " метров.");
         } else {
             System.out.println(name + " пробежал " + range + " метров.");
@@ -19,10 +23,10 @@ public class Dog extends Animal{
  
     @Override
     public void swim(int range) {
-        if (range > 10) {
+        if (range > maxSwim) {
             System.out.println("Собака не может проплыть " + range + " метров.");
         } else {
             System.out.println(name + " проплыл " + range + " метров.");
         }
     }
-}    
+}   
