9cb4962ef1b6b4973b756a48f25ec5c9e020320e
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-02 19:56:45 +0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,4 +1,4 @@
-class Triangle implements Shape {
+iclass Triangle implements Perimeter, Area {
     private double side1;
     private double side2;
     private double side3;
@@ -11,10 +11,12 @@ class Triangle implements Shape {
         this.side3 = side3;
     }
 
+    @Override
     public double calculatePerimeter() {
         return side1 + side2 + side3;
     }
 
+    @Override
     public double calculateArea() {
         double halfPerimeter = calculatePerimeter() / 2;
         return Math.sqrt(halfPerimeter * (halfPerimeter - side1) * (halfPerimeter - side2) * (halfPerimeter - side3));
