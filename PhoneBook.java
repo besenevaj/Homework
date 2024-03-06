d3693ae58b1e86bb631e779f214fcbf41e2f39d6
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-06 17:19:17 +0400

    Create PhoneBook.java

---
+++ PhoneBook.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PhoneBook {
+    private final HashMap<String, ArrayList<String>> items = new HashMap<>();
+
+    public void add(String surname, String phoneNumber) {
+        if (items.get(surname) != null) {
+            if (!items.get(surname).contains(phoneNumber)) {
+                items.get(surname).add(phoneNumber);
+            }
+        } else {
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add(phoneNumber);
+            items.put(surname, arr);
+        }
+    }
+
+    public void get(String surname) {
+        System.out.println(surname + "\nтелефон(ы): ");
+        System.out.println(items.get(surname));
+    }
+}
