fbcf7767808da09d5a478fd5c17bbcc78433b2e8
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-06 17:18:11 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
-
+public class Main {
+    public static void main(String[] args){
+        Array.uniqueWords();
+        System.out.println();
+        PhoneBook pB = new PhoneBook();
+        pB.add("Петров", "+79011234567");
+        pB.add("Иванов", "+79025674321");
+        pB.add("Петров", "+79998763512");
+        pB.get("Петров");
+        pB.get("Иванов");
+        }
+    }
