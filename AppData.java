d943c3cc179e9d444bd7452bf894d774a94ba2b7
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-09 19:37:18 +0400

    Update AppData.java

--- AppData.java
+++ AppData.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
 public class AppData {
-    private final String[] headers;
-    private String[] header;
+    private String[] headers;
     private int[][] data;
 
+    public AppData() {}
     public AppData(String[] headers, int[][] data) {
         this.headers = headers;
         this.data = data;
@@ -12,26 +12,47 @@ public class AppData {
 
     @Override
     public String toString() {
-        long[] headers = new long[0];
-        return "AppData{" +
-                "headers=" + Arrays.toString(headers) +
-                ", data=" + dataToString() +
-                '}';
+        return toCsv();
     }
-
-    private String dataToString() {
-        StringBuilder builder = new StringBuilder("[");
-        for (int[] values : data) {
-            builder.append(dataValuesToString(values)).append(",");
+    
+    public String toCsv() {
+        String[] rows = new String[1 + data.length];
+        
+        rows[0] = String.join(";", headers);
+        
+        for(int i = 0; i < data.length; ++i) {
+            String[] values = new String[data[i].length];
+            for(int j = 0; j < data[i].length; ++j) {
+                values[j] = Integer.toString(data[i][j]);
+            }
+            
+            rows[1 + i] = String.join(";", values);
         }
-        return builder.delete(builder.length() - 1, builder.length()).append("]").toString();
+        
+        return String.join("\n", rows);
     }
-
-    private String dataValuesToString(int[] values) {
-        StringBuilder builder = new StringBuilder("[");
-        for (int intValue : values) {
-            builder.append(intValue).append(",");
+    
+    public void fromCsv(String csv_data) throws Exception {
+        String[] rows = csv_data.split("\n");
+        if(rows.length == 0) {
+            throw new Exception("Not enough data in string");
         }
-        return builder.delete(builder.length() - 1, builder.length()).append("]").toString();
+        
+        headers = rows[0].split(";");
+        for(int i = 0; i < headers.length; ++i) {
+            headers[i] = headers[i].trim();
+        }
+        
+        data = new int[rows.length - 1][headers.length];
+        for(int i = 1; i < rows.length; ++i) {
+            String[] row = rows[i].split(";");
+            if(row.length != headers.length) {
+                throw new Exception("Row length does not match headers");
+            }
+            
+            for(int j = 0; j < row.length; ++j) {
+                data[i - 1][j] = Integer.parseInt(row[j].trim());
+            }
+        }
     }
 }
