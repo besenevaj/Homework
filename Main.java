cdfc30aa2ca2c3c5c77f0742c0cf136a99058948
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-10 23:30:07 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
-
+public class Main {
+    public static void main(String[] args) {
+    EvenNumbers.print();
+    System.out.println();
+    Highload.print();
+    System.out.println();
+    CollectionSorted.print();
+    System.out.println();
+    Students.print();
+    System.out.println();
+    Logins.parse();
+  }
+}
