ee6a2a4a14d06dd727a609f5fa902df8f42e8e21
besenevaj <159151572+besenevaj@users.noreply.github.com>
2024-03-14 14:04:31 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,35 @@
+class Fruit {
+}
 
+class Apple extends Fruit {
+}
+
+class Orange extends Fruit {
+}
+
+public class Main {
+    public static void main(String[] args){
+    Box<Apple> box1 = new Box<>();
+    box1.add(new Apple());
+    box1.add(new Apple());
+    box1.add(new Apple());
+    
+    Box<Orange> box2 = new Box<>();
+    box2.add(new Orange());
+    box2.add(new Orange());
+    
+    System.out.println(box1.getWeight());
+    System.out.println(box2.getWeight());
+
+    System.out.println(box1.compare(box2));
+    
+    Box<Apple> box3 = new Box<>();
+    box3.add(new Apple());
+    box3.add(new Apple());
+    
+    box1.pourTo(box3);
+    
+    System.out.println(box1.getWeight());
+    System.out.println(box3.getWeight());
+    }
+}
